cfe88c5d85e0b169bd690b4e39ca6769dca8ccc2
Yaroslav Polyakov <polyakov.production@gmail.com>
2017-03-22 17:52:32 +0200

    aspect ratio preview view

--- sample/src/main/java/com/steelkiwi/cropiwa/sample/adapter/AspectRatioPreviewAdapter.java
+++ sample/src/main/java/com/steelkiwi/cropiwa/sample/adapter/AspectRatioPreviewAdapter.java
@@ -1,4 +1,99 @@
 package com.steelkiwi.cropiwa.sample.adapter;
 
-public class AspectRatioPreviewAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.steelkiwi.cropiwa.AspectRatio;
+import com.steelkiwi.cropiwa.sample.R;
+import com.steelkiwi.cropiwa.sample.view.AspectRatioPreviewView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AspectRatioPreviewAdapter extends RecyclerView.Adapter<AspectRatioPreviewAdapter.ViewHolder> {
+
+    private AspectRatioPreviewView lastSelectedView;
+    private AspectRatio selectedRatio;
+
+    private List<AspectRatio> ratios;
+
+    private OnNewSelectedListener listener;
+
+    public AspectRatioPreviewAdapter() {
+        ratios = Arrays.asList(
+                new AspectRatio(3, 2),
+                new AspectRatio(4, 3),
+                new AspectRatio(5, 4),
+                new AspectRatio(1, 1),
+                new AspectRatio(4, 5),
+                new AspectRatio(3, 4),
+                new AspectRatio(2, 3));
+        selectedRatio = ratios.get(3);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.item_aspect_ratio, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        AspectRatio ratio = ratios.get(position);
+        holder.ratioView.setAspectRatio(ratio);
+        if (ratio.equals(selectedRatio)) {
+            lastSelectedView = holder.ratioView;
+            holder.ratioView.setSelected(true);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return ratios.size();
+    }
+
+    public void setListener(OnNewSelectedListener listener) {
+        this.listener = listener;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private AspectRatioPreviewView ratioView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ratioView = (AspectRatioPreviewView) itemView.findViewById(R.id.aspect_ratio_preview);
+            ratioView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (lastSelectedView == ratioView) {
+                selectedRatio = ratioView.getRatio();
+                return;
+            }
+            if (ratioView.getRatio().equals(selectedRatio)) {
+                return;
+            }
+            if (lastSelectedView != null) {
+                lastSelectedView.setSelected(false);
+            }
+            ratioView.setSelected(true);
+
+            selectedRatio = ratioView.getRatio();
+            lastSelectedView = ratioView;
+
+            if (listener != null) {
+                listener.onNewAspectRatioSelected(selectedRatio);
+            }
+        }
+    }
+
+    public interface OnNewSelectedListener {
+        void onNewAspectRatioSelected(AspectRatio ratio);
+    }
 }
+
--- sample/src/main/java/com/steelkiwi/cropiwa/sample/view/AspectRatioPreviewView.java
+++ sample/src/main/java/com/steelkiwi/cropiwa/sample/view/AspectRatioPreviewView.java
@@ -1,5 +1,149 @@
 package com.steelkiwi.cropiwa.sample.view;
 
 
-public class AspectRatioPreviewView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.steelkiwi.cropiwa.AspectRatio;
+import com.steelkiwi.cropiwa.sample.R;
+import com.steelkiwi.cropiwa.util.ResUtil;
+import com.yarolegovich.mp.util.Utils;
+
+import java.util.Locale;
+
+public class AspectRatioPreviewView extends View {
+
+    private static final int SIZE_TEXT = 12;
+
+    private final int colorRectNotSelected;
+    private final int colorRectSelected;
+    private final int colorText;
+
+    private AspectRatio ratio;
+    private boolean isSelected;
+
+    private Paint rectPaint;
+    private Paint textPaint;
+
+    private float centerX;
+
+    private RectF previewRect;
+    private Rect textOutBounds;
+
+    public AspectRatioPreviewView(Context context) {
+        super(context);
+    }
+
+    public AspectRatioPreviewView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AspectRatioPreviewView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public AspectRatioPreviewView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    {
+        textOutBounds = new Rect();
+
+        ResUtil r = new ResUtil(getContext());
+        colorRectSelected = r.color(R.color.colorAccent);
+        colorRectNotSelected = r.color(R.color.aspectRatioNotSelected);
+        colorText = r.color(R.color.aspectRatioText);
+
+        rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rectPaint.setColor(colorRectNotSelected);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(Utils.dpToPixels(getContext(), 2));
+
+        DisplayMetrics dm = getContext().getResources().getDisplayMetrics();
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, SIZE_TEXT, dm));
+        textPaint.setColor(colorText);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = w * 0.5f;
+        if (ratio != null) {
+            configurePreviewRect();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (previewRect != null) {
+            canvas.drawRect(previewRect, rectPaint);
+            String text = getAspectRatioString();
+            textPaint.getTextBounds(text, 0, text.length(), textOutBounds);
+            canvas.drawText(
+                    text, centerX - textOutBounds.width() * 0.5f,
+                    getBottom() - textOutBounds.height() * 0.5f,
+                    textPaint);
+        }
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+        rectPaint.setColor(selected ? colorRectSelected : colorRectNotSelected);
+        invalidate();
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setAspectRatio(AspectRatio ratio) {
+        this.ratio = ratio;
+        if (getWidth() != 0 && getHeight() != 0) {
+            configurePreviewRect();
+        }
+        invalidate();
+    }
+
+    public AspectRatio getRatio() {
+        return ratio;
+    }
+
+    private void configurePreviewRect() {
+        String str = getAspectRatioString();
+        textPaint.getTextBounds(str, 0, str.length(), textOutBounds);
+        RectF freeSpace = new RectF(0, 0, getWidth(), getHeight() - textOutBounds.height() * 1.2f);
+
+        boolean calculateFromWidth =
+                ratio.getHeight() < ratio.getWidth()
+                        || (ratio.isSquare() && freeSpace.width() < freeSpace.height());
+
+        float halfWidth, halfHeight;
+        if (calculateFromWidth) {
+            halfWidth = freeSpace.width() * 0.8f * 0.5f;
+            halfHeight = halfWidth / ratio.getRatio();
+        } else {
+            halfHeight = freeSpace.height() * 0.8f * 0.5f;
+            halfWidth = halfHeight * ratio.getRatio();
+        }
+
+        previewRect = new RectF(
+                freeSpace.centerX() - halfWidth, freeSpace.centerY() - halfHeight,
+                freeSpace.centerX() + halfWidth, freeSpace.centerY() + halfHeight);
+    }
+
+    private String getAspectRatioString() {
+        if (ratio == null) {
+            return "";
+        } else {
+            return String.format(Locale.US, "%d:%d", ratio.getWidth(), ratio.getHeight());
+        }
+    }
 }
