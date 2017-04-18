2f17d454866e30c2d5b23bc826ee78f0febc186c
Paul Austian Alday <paul.austian.alday@gmail.com>
2017-04-18 16:43:23 +0800

    added setting output size in pixels

--- library/src/main/java/com/steelkiwi/cropiwa/config/CropIwaSaveConfig.java
+++ library/src/main/java/com/steelkiwi/cropiwa/config/CropIwaSaveConfig.java
@@ -14,6 +14,7 @@ public class CropIwaSaveConfig {
     private Bitmap.CompressFormat compressFormat;
     private int quality;
     private int width, height;
+    private int widthPx, heightPx;
     private Uri dstUri;
 
     public CropIwaSaveConfig(Uri dstPath) {
@@ -21,6 +22,8 @@ public class CropIwaSaveConfig {
         this.compressFormat = Bitmap.CompressFormat.PNG;
         this.width = CropIwaBitmapManager.SIZE_UNSPECIFIED;
         this.height = CropIwaBitmapManager.SIZE_UNSPECIFIED;
+        this.widthPx = CropIwaBitmapManager.SIZE_UNSPECIFIED;
+        this.heightPx = CropIwaBitmapManager.SIZE_UNSPECIFIED;
         this.quality = 90;
     }
 
@@ -44,6 +47,14 @@ public class CropIwaSaveConfig {
         return dstUri;
     }
 
+    public int getWidthPx(){
+        return widthPx;
+    }
+
+    public int getHeightPx(){
+        return heightPx;
+    }
+
     public static class Builder {
 
         private CropIwaSaveConfig saveConfig;
@@ -65,6 +76,12 @@ public class CropIwaSaveConfig {
 
         public Builder setQuality(@IntRange(from = 0, to = 100) int quality) {
             saveConfig.quality = quality;
+            return this;
+        }
+
+        public Builder setSizeInPx(int width, int height){
+            saveConfig.widthPx = width;
+            saveConfig.heightPx = height;
             return this;
         }
 
